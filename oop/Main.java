7af1b56641c5c05b479c17e9fffcf441a76fbc3c
Anh Quan <quan.nguyen2412899@hcmut.edu.vn>
2026-03-23 13:45:20 +0700

    implement Main

--- oop/Main.java
+++ oop/Main.java
@@ -1,33 +1,85 @@
 import core.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import models.Population;
 
 /**
  * Usage: java Main --config <json config file> --problem <json problem file> --out <output directory>
  * Example: java Main --config config.json --problem onemax.json --out reports
+ * There's no default argument, user must declare all flags.
+ * <output directory>'s existence is user's responsibility, the program doesn't automatically create one if not found.
  **/
 public class Main {
+
+    private static final String USAGE_MESSAGE = "Usage: java Main --config <json config file> --problem <json problem file> --out <output directory>";
+
     public static void main(String[] args) {
+        
+        /** Parsing & argument safe guards **/
+        Parser p = new Parser(args);
+        if(!p.isValid) errorExit("Invalid argument.");
+        String generalConfigFile = p.generalConfigFile;
+        String problemConfigFile = p.problemConfigFile;
+        String outputDirectory = p.outputDirectory;
 
-        /* get configs from args - incomplete implementation */
-        String generalConfigFile = "";
-        String problemConfigFile = "";
-        String outputDirectory = "";
+        try {
+            /** Initialization  **/
+            GAConfig config = new GAConfig(generalConfigFile, problemConfigFile);
+            GeneticAlgorithm ga = new GeneticAlgorithm(config.fitnessEvaluator(),
+                                                        config.elitismStrategy(),
+                                                        config.selectionStrategy(),
+                                                        config.crossoverStrategy(),
+                                                        config.mutationStrategy(),
+                                                        config.randomGenerator());
+            EvolutionReporter reporter = new EvolutionReporter();
 
-        /** Initialization  **/
-        GAConfig config = new GAConfig(generalConfigFile, problemConfigFile);
-        GeneticAlgorithm ga = new GeneticAlgorithm(config.fitnessEvaluator(),
-                                                    config.elitismStrategy(),
-                                                    config.selectionStrategy(),
-                                                    config.crossoverStrategy(),
-                                                    config.mutationStrategy(),
-                                                    config.randomGenerator());
-        EvolutionReporter reporter = new EvolutionReporter();
+            /** Execution **/
+            Population initialPopulation = config.generateRandomPopulation();
+            ga.run(initialPopulation, reporter);
 
-        /** Execution **/
-        Population initialPopulation = config.generateRandomPopulation();
-        ga.run(initialPopulation, reporter);
+            /** Results exportation **/
+            reporter.exportStatistics(outputDirectory);
+        } 
+        catch(Exception e) {
+            errorExit(e.getMessage());
+        }
+    }
 
-        /** Export results **/
-        reporter.exportStatistics(outputDirectory);
+    /** Argument parser helper class **/
+    private static class Parser {
+        private String generalConfigFile, problemConfigFile, outputDirectory;
+        private boolean isValid;
+        public Parser(String[] args) {
+            isValid = true;
+            if(args.length != 6) isValid = false;
+            for (int i = 0; i < args.length - 1 && isValid; i += 2) {
+                switch (args[i]) {
+                    case "--config" -> generalConfigFile = args[i+1];
+                    case "--problem" -> problemConfigFile = args[i+1];
+                    case "--out" -> outputDirectory = args[i+1];
+                    default -> isValid = false;
+                }
+            }
+            fileExistenceCheck();
+            fileTypeCheck();
+        }
+        private void fileExistenceCheck() {
+            if (!isValid) return;
+            if (!Files.exists(Paths.get(generalConfigFile))) isValid = false;
+            if (!Files.exists(Paths.get(problemConfigFile))) isValid = false;
+            if (!Files.exists(Paths.get(outputDirectory))) isValid = false;
+        }
+        private void fileTypeCheck() {
+            if (!isValid) return;
+            if (!generalConfigFile.endsWith(".json")) isValid = false;
+            if (!problemConfigFile.endsWith(".json")) isValid = false;
+        }
+    }
+
+    /** On error **/
+    private static void errorExit(String msg) {
+        System.err.println(msg);
+        System.err.println(USAGE_MESSAGE);
+        System.exit(1);
     }
 }
