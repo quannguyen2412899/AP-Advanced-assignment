0a4c452b48938ca6381833ab15718664004c9047
Anh Quan <quan.nguyen2412899@hcmut.edu.vn>
2026-04-21 23:36:27 +0700

    remove random_int(start,end)

--- fp/src/random_util.py
+++ fp/src/random_util.py
@@ -8,17 +8,17 @@ def random_gen(*keys) -> int:
     input_str = ":".join(map(str, keys)).encode()
     return int.from_bytes(hashlib.sha256(input_str).digest(), 'big')
 
-def random_int(start: int, end: int ,*keys) -> int:
-    # [start, end)
-    if start >= end:
-        raise ValueError()
-    return random_gen(*keys) % (end - start) + start
+# def random_int(start: int, end: int ,*keys) -> int:
+#     # [start, end)
+#     if start >= end:
+#         raise ValueError()
+#     return random_gen(*keys) % (end - start) + start
 
 def random_int(bound: int, *keys) -> int:
     # [start, end)
     if bound < 0:
         raise ValueError()
-    return random_int(0, bound, *keys)
+    return random_gen(*keys) % bound
 
 def random_double(start: float, end: float, *keys) -> float:
     # [start, end)
@@ -29,4 +29,4 @@ def random_double(start: float, end: float, *keys) -> float:
 def random_bernoulli(p: float, *keys) -> bool:
     if p < 0 or p > 1:
         raise ValueError(f"Probability p must be between 0 and 1, got {p}")
-    return random_double(0, 1) < p
+    return random_double(0, 1, *keys) < p
