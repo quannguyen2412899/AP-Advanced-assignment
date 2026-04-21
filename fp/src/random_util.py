53c1d825e794e0101e48da7443be7fda041ddff0
Anh Quan <quan.nguyen2412899@hcmut.edu.vn>
2026-04-21 16:43:16 +0700

    implement hase-based random generator

---
+++ fp/src/random_util.py
@@ -0,0 +1,32 @@
+import hashlib
+
+def random_gen(*keys) -> int:
+    """
+    Returns a deterministic, stateless 'random' float [0, 1) 
+    based on any number of provided keys.
+    """
+    input_str = ":".join(map(str, keys)).encode()
+    return int.from_bytes(hashlib.sha256(input_str).digest(), 'big')
+
+def random_int(start: int, end: int ,*keys) -> int:
+    # [start, end)
+    if start >= end:
+        raise ValueError()
+    return random_gen(*keys) % (end - start) + start
+
+def random_int(bound: int, *keys) -> int:
+    # [start, end)
+    if bound < 0:
+        raise ValueError()
+    return random_int(0, bound, *keys)
+
+def random_double(start: float, end: float, *keys) -> float:
+    # [start, end)
+    if start >= end:
+        raise ValueError()
+    return random_gen(*keys) * (end - start) / 2**256 + start
+
+def random_bernoulli(p: float, *keys) -> bool:
+    if p < 0 or p > 1:
+        raise ValueError(f"Probability p must be between 0 and 1, got {p}")
+    return random_double(0, 1) < p
